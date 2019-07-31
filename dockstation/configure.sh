64c0f933c71c98a3b5126439c0426ccee0058aa5
m-lewinski <lewin.michal@gmail.com>
2019-07-31 12:13:09 +0200

    improved audio and i3 key bindings

--- dockstation/configure.sh
+++ dockstation/configure.sh
@@ -3,7 +3,7 @@
 SCRIPT="hotplug_dock.sh"
 
 
-echo "KERNEL==\"card0\", ACTION==\"change\", SUBSYSTEM==\"drm\", ENV{DISPLAY}=\":0\", ENV{XAUTHORITY}=\"/home/$USER/.Xauthority\", RUN+=\"/home/$USER/.dotifles/dockstation/$SCRIPT\"" | sudo tee /etc/udev/rules.d/90-thinkpad-dock.conf
+echo "KERNEL==\"card0\", ACTION==\"change\", SUBSYSTEM==\"drm\", ENV{DISPLAY}=\":0\", ENV{XAUTHORITY}=\"/home/$USER/.Xauthority\", RUN+=\"/home/$USER/.dotfiles/dockstation/$SCRIPT\"" | sudo tee /etc/udev/rules.d/90-thinkpad-dock.conf
 
 
 
